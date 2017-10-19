65d0c3087a3c760f67e4ff210acb1c6d814b9082
Marc Poulhiès <marc.poulhies@kalray.eu>
2017-10-19 18:03:06 +0200

    [K1] fix register spilling
         * k1.c (k1_emit_single_spill): fix handling of $ra that maybe
         shorter than a register, making the single spill variable size.

--- gcc/config/k1/k1.c
+++ gcc/config/k1/k1.c
@@ -1882,8 +1882,7 @@ k1_emit_single_spill (int regno, enum spill_action action, int *offset, int *sta
 {
     rtx reg, insn, mem, base = stack_pointer_rtx;
     /* enum machine_mode spill_mode = (regno == K1C_RETURN_POINTER_REGNO) ? Pmode : SImode; */
-    /* gcc_assert(spill_mode == DImode); */
-    enum machine_mode spill_mode = DImode;
+    enum machine_mode spill_mode = (regno == K1C_RETURN_POINTER_REGNO) ? Pmode : DImode;
 
     if (action != SPILL_COMPUTE_SIZE) {
 	reg = gen_rtx_REG (spill_mode, regno);
@@ -1919,8 +1918,8 @@ k1_emit_single_spill (int regno, enum spill_action action, int *offset, int *sta
 	}
     }
 
-    *offset += 4;
-    *stack_size += 4;
+    *offset += GET_MODE_SIZE(spill_mode);
+    *stack_size += GET_MODE_SIZE(spill_mode);
 }
 
 static void
