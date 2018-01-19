361117d113e1a766a29cb8820982b3fcf47fb4fb
Marc Poulhiès <marc.poulhies@kalray.eu>
2018-01-19 10:52:33 +0100

    [K1] New tiny-k1 multilib and needed changes.
    Having the tiny-k1 mode working needed some fiddling in libgcc and
    some backend patterns. Some old and unused code has been piggybacked
    to /dev/null. This has also been used as an opportunity to refactor
    some patterns.
    
    	* k1/iterators.md (ALLF,SISIZE,SISIZESCALAR,ALLMODES): Added guard
    	to filter out float when mtiny-k1 used.
    	(ALLMF): Same as ALLF but without guard to allow moving float even
    	when mtiny-k1 is used.
    	(ANY_ZERO_COUNT,c_tl): Iterators to factorize ctz/clz patterns.
    	* k1/k1.c (machine_function): Remove unused fake_SC field.
    	(k1_isa_filter_enabled_p): New function used to filter out
    	disabled instructions.
    	* k1/k1.h (ASM_SPEC): pass -mtiny-k1 to assembler.
    	(fake_SC): Remove.
    	* k1/k1.md (mov<mode>reg_k1c): Used ALLMF instead of ALLF.
    	(sub<mode>3_x2,sub<mode>3_mult_x2,sub<mode>3_x4,sub<mode>3_mult_x4)
    	(sub<mode>3_x8,sub<mode>3_mult_x8,sub<mode>3_x16)
    	(sub<mode>3_mult_x16): Add isa filter for -mtiny-k1
    	(clzsi2,ctzdi2,clzdi2,ctzsi2):Remove.
    	(c<c_tl>z<mode>2): New.
    	(div<mode>3): Disable until MAU/FPU reworked.
    	(fcomp,fcompd): Removed.
    	(fcomp<sfx>): New refactored fcomp.
    	(fsuffix): Remove iterator, redundant with other.
    	(cbranch<mode>4): Filter out in tiny-k1 mode.
    	* k1/t-elf (MULTILIB_OPTIONS,MULTILIB_DIRNAMES): Add tiny-k1
    	multilib.
    	* libgcc/config/k1/udivmoddi4.c (udivmoddi4): Provide a tiny-k1
    	compatible version (and consequently less optimal).
    	* libgcc/config.host (k1): Remove k1/t-k1-softfp as it is not
    	useful anymore (comment points to initial version of first K1
    	core).
    	* libgcc/config/k1/t-k1-softfp: Likewise.
    	* libgcc/config/k1/t-k1: Restrict a bit what is overriden from
    	libgcc. This optimised code must be reworked to be really useful.

--- libgcc/config/k1/udivmoddi4.c
+++ libgcc/config/k1/udivmoddi4.c
@@ -1,11 +1,39 @@
+#ifdef __K1_TINYK1__
+static unsigned long long
+udivmoddi4(unsigned long long num, unsigned long long den, int modwanted)
+{
+  unsigned long long bit = 1;
+  unsigned long long res = 0;
 
+  while (den < num && bit && !(den & (1L<<31)))
+    {
+      den <<=1;
+      bit <<=1;
+    }
+  while (bit)
+    {
+      if (num >= den)
+	{
+	  num -= den;
+	  res |= bit;
+	}
+      bit >>=1;
+      den >>=1;
+    }
+  if (modwanted) return num;
+  return res;
+}
+
+#else
+
+/* THIS IS THE PREVIOUS VERSION, USED ON BOSTAN AND ANDEY */
 static unsigned long long
 udivmoddi4(unsigned long long num, unsigned long long den, int modwanted)
 {
     unsigned long long r = num, q = 0;
 
     if(den <= r) {
-	unsigned k = __builtin_k1_clzdl (den) - __builtin_k1_clzdl (r);
+	unsigned k = __builtin_clz (den) - __builtin_clz (r);
 	den = den << k;
 	if(r >= den) {
 	    r = r - den;
@@ -26,6 +54,7 @@ udivmoddi4(unsigned long long num, unsigned long long den, int modwanted)
 
     return modwanted ? r : q;
 }
+#endif	/* __K1_TINYK1__ */
 
 unsigned long long
 __udivdi3 (unsigned long long a, unsigned long long b)
