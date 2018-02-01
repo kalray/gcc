4c9462d0c578c68a779608d6b4455cd0f982d547
Julien Villette <jvillette@kalray.eu>
2018-02-01 15:02:03 +0100

    [K1] Fix hfxb and hfxt macros
        * k1/builtin_k1.h: unsigned long long cast was not in the right place.

--- gcc/config/k1/builtin_k1.h
+++ gcc/config/k1/builtin_k1.h
@@ -64,8 +64,8 @@ extern unsigned long long __builtin_k1_get(int);
 /* GOTO */
 /* WFXL */
 extern void __builtin_k1_wfxl(unsigned char, long long);
-#define __builtin_k1_hfxb(sfr, argument) { __builtin_k1_wfxl(sfr, ((unsigned long long)(argument>>16)<<32)|((unsigned long long)(argument&0xFFFF))); }
-#define __builtin_k1_hfxt(sfr, argument) { __builtin_k1_wfxl(sfr, ((unsigned long long)(argument>>16)<<48)|((unsigned long long)(argument&0xFFFF)<<16)); }
+#define __builtin_k1_hfxb(sfr, argument) { __builtin_k1_wfxl(sfr, (((unsigned long long)argument>>16)<<32)|(((unsigned long long)argument&0xFFFF))); }
+#define __builtin_k1_hfxt(sfr, argument) { __builtin_k1_wfxl(sfr, (((unsigned long long)argument>>16)<<48)|(((unsigned long long)argument&0xFFFF)<<16)); }
 #define __builtin_k1_hfx(arg1,arg2,arg3,arg4)	\
   switch(arg1) { \
   case 0: __builtin_k1_hfxb(arg2,(((int)(arg3)<<16) | ((int)(arg4)&0xffff))); break; \
