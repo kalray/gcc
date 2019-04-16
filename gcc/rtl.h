e0656153a890732118529c281602253a2337867f
Benoit Dinechin <benoit.dinechin@kalray.eu>
2019-04-16 12:28:40 +0200

    Revert use of generating_concat_minsize in generic GCC code.
      * rtl.h, rtl.c, emit-rtl.c, k1.c: remove line.
      * complex_float_double.c: disable complex intrinsics calls.

--- gcc/rtl.h
+++ gcc/rtl.h
@@ -2651,7 +2651,6 @@ do {								        \
 
 /* Nonzero when we are generating CONCATs.  */
 extern int generating_concat_p;
-extern int generating_concat_minsize;
 
 /* Nonzero when we are expanding trees to RTL.  */
 extern int currently_expanding_to_rtl;
