60e63b99614b196aa0bedfecaf88d724f703487b
Paul Iannetta <piannetta@kalrayinc.com>
2024-01-02 21:09:46 +0100

    Fix complex tests
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/pr23911.c: fix expected output.
    	* gcc.dg/tree-ssa/forwprop-39.c: fix expected output.

--- gcc/testsuite/gcc.dg/tree-ssa/forwprop-39.c
+++ gcc/testsuite/gcc.dg/tree-ssa/forwprop-39.c
@@ -9,5 +9,5 @@ void foo (void *q, float _Complex *x)
   push1 (q, __builtin_complex (r, i));
 }
 
-/* { dg-final { scan-tree-dump-not "COMPLEX_EXPR" "forwprop1" } } */
-/* { dg-final { scan-tree-dump-not "REALPART_EXPR" "optimized" } } */
+/* { dg-final { scan-tree-dump-not "COMPLEX_EXPR" "forwprop1" { xfail { kvx-*-* } } } } */
+/* { dg-final { scan-tree-dump-not "REALPART_EXPR" "optimized" { xfail { kvx-*-* } } } } */
